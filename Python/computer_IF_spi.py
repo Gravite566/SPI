c6cb57bd0ef937210f764dc88ab7a98d13baf6ba
tvarin002 <thomas@LAPTOP-AOPIP7CJ>
2024-03-04 16:48:02 +0100

    1ère version de computer IF avec une class pour la communication SPI et une pour la communication UART. La class Resp_msg n'a pas été modifiée

--- Python/computer_IF_spi.py
+++ Python/computer_IF_spi.py
@@ -19,6 +19,7 @@
 #    FIFO read    : a[addr:len]
 
 
+import serial
 import spidev
 
 ACK_Nothing_To_Say      = 0x00
@@ -51,7 +52,9 @@ OPCODE_PCKT_READ    = 0xD0
 OPCODE_PCKT_WRITE   = 0xE0
 OPCODE_INVALID      = 0xF0
 
-#il faut modifier tout ce qui fait appel à serial et remplacer par la librairie spidev
+
+    
+    
 class Resp_msg:
     def __init__(self, device, expected):
         self.device      = device
@@ -59,14 +62,14 @@ class Resp_msg:
         r = self.device.serial.read(1)[0]
         self.expected    = expected #code ACK attendu
         self.received    = r        #code ACK réellement reçu
-        self.ok          = (r & MASK_Resp_Code) == expected #True or False en fonction de si on reçoit ce que l'on veut
+        self.ok          = (r & MASK_Resp_Code) == expected #True or False en fonction de si on reçoit ce que l'on veu
         self.address_err = (r & MASK_Resp_Code) == ACK_Address_Error
         self.timeout_err = (r & MASK_Resp_Code) == ACK_Timeout
         self.nullsize    = (r & MASK_Resp_Code) == ACK_Null_Size
         self.invalid_err = (r & MASK_Resp_Code) == ACK_Invalid_Instruction
         self.FIFO_err    = ((r & MASK_Fifo_err) != 0)
         if (r & MASK_Resp_Code) == ACK_Single:
-            d = self.device.spidev.read(2)
+            d = self.device.serial.read(2)
             self.data = d[0]*256+d[1]
         else:
             self.data = None
@@ -103,8 +106,252 @@ class Resp_msg:
         if self.invalid_err:
             return NotImplementedError
         return None
-    
-class Device: 
-    def __init__(self, dev, speed=1000000):
-        self.spidev = spidev.Spidev()
         
+        
+
+
+class Device_uart:
+    def __init__(self, dev, speed=921600):
+        self.serial = serial.Serial(dev, speed)
+        self.serial.write(bytes([OPCODE_READ_IFVER]))
+        self.IFver = Resp_msg(self, ACK_Single).data
+        self.serial.write(bytes([OPCODE_READ_PROJID]))
+        self.ProjID = Resp_msg(self, ACK_Single).data
+
+    def _write_single(self, data, addr):
+        self.serial.write(bytes([OPCODE_WRITE_SINGLE, (addr//256)%256, addr%256, (data//256)%256, data%256]))
+        return Resp_msg(self, ACK_Short)
+        
+    def _read_single(self, addr):
+        self.serial.write(bytes([OPCODE_READ_SINGLE, (addr//256)%256, addr%256]))
+        return Resp_msg(self, ACK_Single)
+
+    def _read_map(self, addr, length):
+        self.serial.write(bytes([OPCODE_READ_MAP, (addr//256)%256, addr%256, (length//256)%256, length%256]))
+        r = Resp_msg(self, ACK_Stream_Start)
+        if r.ok:
+            dat = self.serial.read(length*2)
+            resp_data = [dat[i]*256+dat[i+1] for i in range(0, length*2, 2)]
+            r.end_of_stream()
+            resp_data = resp_data[:r.dataok]
+            return resp_data
+        return []
+
+    def _read_FIFO(self, addr, length):
+        self.serial.write(bytes([OPCODE_READ_FIFO, (addr//256)%256, addr%256, (length//256)%256, length%256]))
+        r = Resp_msg(self, ACK_Stream_Start)
+        if r.ok:
+            dat = self.serial.read(length*2)
+            resp_data = [dat[i]*256+dat[i+1] for i in range(0, length*2, 2)]
+            r.end_of_stream()
+            resp_data = resp_data[:r.dataok]
+            return resp_data
+        return []
+
+    def _write_map(self, addr, data):
+        length = len(data)
+        self.serial.write(bytes([OPCODE_WRITE_MAP, (addr//256)%256, addr%256, (length//256)%256, length%256, (data[0]//256)%256, data[0]%256]))
+        r = Resp_msg(self, ACK_Short)
+        if r.ok:
+            dat = [i.to_bytes(2, byteorder='big') for i in data[1:]]
+            self.serial.write(b''.join(dat))
+            r.end_of_stream()
+        return r
+
+    def _write_FIFO(self, addr, data):
+        length = len(data)
+        self.serial.write(bytes([OPCODE_WRITE_FIFO, (addr//256)%256, addr%256, (length//256)%256, length%256, (data[0]//256)%256, data[0]%256]))
+        r = Resp_msg(self, ACK_Short)
+        if r.ok:
+            dat = [i.to_bytes(2, byteorder='big') for i in data[1:]]
+            self.serial.write(b''.join(dat))
+            r.end_of_stream()
+        return r
+
+    def _nop(self):
+        self.serial.write(bytes([OPCODE_NOP]))
+        return Resp_msg(self, ACK_Short)
+
+    def __getitem__(self, addr):
+        if isinstance(addr, slice):
+            if addr.start is None :
+                 raise Exception("Need the base address for read operation")
+            if addr.step is None or addr.step == 1:
+                if addr.stop is None:
+                    raise Exception("Need a stop address (None provided)")
+                return self._read_map(addr.start, addr.stop - addr.start)
+            elif addr.step == 0:
+                if addr.stop is None:
+                    raise Exception("Need a number of elements as stop address (None provided)")
+                return self._read_FIFO(addr.start, addr.stop)
+            else:
+                #collection of single read
+                raise Exception("step values other than 0 or 1 not supported yet")
+        r = self._read_single(addr)
+        if r.ok:
+            return r.data
+        return r.exception()
+
+    def __setitem__(self, addr, data):
+        if isinstance(addr, slice):
+            if addr.start is None:
+                raise Exception("Need the base address for read operation")
+            if isinstance(data, int):
+                if addr.step is None or addr.step == 1:
+                    if addr.stop is None:
+                        raise Exception("Need a stop address in this context")
+                    r = self._write_map(addr.start, [data]*(addr.stop - addr.start))
+                    return
+                elif addr.step == 0:
+                    r = self._write_FIFO(addr.start, [data]*addr.stop)
+                    return
+            if not isinstance(data, list):
+                raise TypeError
+            if addr.step is None or addr.step == 1:
+                if addr.stop is None or addr.stop == addr.start + len(data):
+                    r = self._write_map(addr.start, data)
+                    if r.ok:
+                        return
+                    raise r.exception()
+                else:
+                    raise Exception("Inconsistent data size and addresses")
+            elif addr.step == 0:
+                if not addr.stop is None and addr.stop != len(data):
+                    raise Exception("Inconsistent data size and access len")
+                r = self._write_FIFO(addr.start, data)
+                if r.ok:
+                    return
+            else:
+                raise Exception("step values other than 0 or 1 not supported yet")
+                
+        if isinstance(data, list):
+            return self._write_FIFO(addr, data)
+
+        return self._write_single(data, addr).exception()
+    
+    
+    
+    
+    
+    
+    
+class Device_spi:
+    def __init__(self, dev, speed=921600):
+        self.spi = spidev.Spidev()
+        self.spi.open(0,0)
+        self.spi.maximum_speed_hz(speed)
+        self.spi.writebytes([OPCODE_READ_IFVER])
+        self.IFver = Resp_msg(self, ACK_Single).data
+        self.spi.writebytes([OPCODE_READ_PROJID])
+        self.ProjID = Resp_msg(self, ACK_Single).data
+
+    def _write_single(self, data, addr):
+        self.spi.writebytes([OPCODE_WRITE_SINGLE, (addr//256)%256, addr%256, (data//256)%256, data%256])
+        return Resp_msg(self, ACK_Short)
+        
+    def _read_single(self, addr):
+        self.spi.writebytes([OPCODE_READ_SINGLE, (addr//256)%256, addr%256])
+        return Resp_msg(self, ACK_Single)
+
+    def _read_map(self, addr, length):
+        self.spi.writebytes([OPCODE_READ_MAP, (addr//256)%256, addr%256, (length//256)%256, length%256])
+        r=Resp_msg(self,ACK_Stream_Start)
+        if r.ok:
+            dat = self.spi.readbytes(length*2)
+            resp_data = [dat[i]*256+dat[i+1] for i in range(0, length*2, 2)]
+            r.end_of_stream()
+            resp_data = resp_data[:r.dataok]
+            return resp_data
+        return []
+
+    def _read_FIFO(self, addr, length):
+        self.spi.writebytes([OPCODE_READ_FIFO, (addr//256)%256, addr%256, (length//256)%256, length%256]) 
+        r = Resp_msg(self, ACK_Stream_Start)
+        if r.ok:
+            dat = self.spi.readbytes(length*2)
+            resp_data = [dat[i]*256+dat[i+1] for i in range(0, length*2, 2)]
+            r.end_of_stream()
+            resp_data = resp_data[:r.dataok]
+            return resp_data
+        return []
+
+    def _write_map(self, addr, data): 
+        length = len(data)
+        self.spi.writebytes([OPCODE_WRITE_MAP, (addr//256)%256, addr%256, (length//256)%256, length%256, (data[0]//256)%256, data[0]%256])
+        r = Resp_msg(self, ACK_Short)
+        if r.ok:
+            dat = [i.to_bytes(2, byteorder='big') for i in data[1:]]
+            self.spi.writebytes([b''.join(dat)])
+            r.end_of_stream()
+        return r
+
+    def _write_FIFO(self, addr, data):
+        length = len(data)
+        self.spi.writebytes([OPCODE_WRITE_FIFO, (addr//256)%256, addr%256, (length//256)%256, length%256, (data[0]//256)%256, data[0]%256])
+        r = Resp_msg(self, ACK_Short)
+        if r.ok:
+            dat = [i.to_bytes(2, byteorder='big') for i in data[1:]]
+            self.spi.writebytes([b''.join(dat)])
+            r.end_of_stream()
+        return r
+
+    def _nop(self):
+        self.spi.writebytes([OPCODE_NOP])
+        return Resp_msg(self, ACK_Short)
+
+    def __getitem__(self, addr):
+        if isinstance(addr, slice):
+            if addr.start is None :
+                 raise Exception("Need the base address for read operation")
+            if addr.step is None or addr.step == 1:
+                if addr.stop is None:
+                    raise Exception("Need a stop address (None provided)")
+                return self._read_map(addr.start, addr.stop - addr.start)
+            elif addr.step == 0:
+                if addr.stop is None:
+                    raise Exception("Need a number of elements as stop address (None provided)")
+                return self._read_FIFO(addr.start, addr.stop)
+            else:
+                #collection of single read
+                raise Exception("step values other than 0 or 1 not supported yet")
+        r = self._read_single(addr)
+        if r.ok:
+            return r.data
+        return r.exception()
+
+    def __setitem__(self, addr, data):
+        if isinstance(addr, slice):
+            if addr.start is None:
+                raise Exception("Need the base address for read operation")
+            if isinstance(data, int):
+                if addr.step is None or addr.step == 1:
+                    if addr.stop is None:
+                        raise Exception("Need a stop address in this context")
+                    r = self._write_map(addr.start, [data]*(addr.stop - addr.start))
+                    return
+                elif addr.step == 0:
+                    r = self._write_FIFO(addr.start, [data]*addr.stop)
+                    return
+            if not isinstance(data, list):
+                raise TypeError
+            if addr.step is None or addr.step == 1:
+                if addr.stop is None or addr.stop == addr.start + len(data):
+                    r = self._write_map(addr.start, data)
+                    if r.ok:
+                        return
+                    raise r.exception()
+                else:
+                    raise Exception("Inconsistent data size and addresses")
+            elif addr.step == 0:
+                if not addr.stop is None and addr.stop != len(data):
+                    raise Exception("Inconsistent data size and access len")
+                r = self._write_FIFO(addr.start, data)
+                if r.ok:
+                    return
+            else:
+                raise Exception("step values other than 0 or 1 not supported yet")
+                
+        if isinstance(data, list):
+            return self._write_FIFO(addr, data)
+
+        return self._write_single(data, addr).exception()
