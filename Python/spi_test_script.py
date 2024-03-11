29e481013ccb098cf4bd63967b664464742d1628
tvarin002 <thomas@LAPTOP-AOPIP7CJ>
2024-03-11 16:45:15 +0100

    les scripts doivent permettre de communiquer. Je me suis servi de ce script pour debugger la version SPI. Maintenant, lorsque j'execute le script, aucune erreur n'apparaît. Cependant, celui-ci ne me permet pas encore de communiquer.

---
+++ Python/spi_test_script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from spi_only import*
+import spidev
+import serial
+
+# Open SPI bus
+computer = Device(dev=None,speed=1000000)
+
+
+    
+# End of script
+print("End of script")
+
+
+    
---
+++ Python/uart_test_script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from uart_only import*
+import serial
+
+# Open SPI bus
+computer = Device(device=1,expected=0)
+
+
+    
+# End of script
+print("End of script")
+
+
+    
+
